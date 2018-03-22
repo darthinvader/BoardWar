75a0153687168585789555515dfe90c157f44bd6
darthinvader <psaradakisJ@gmail.com>
2018-03-22 20:13:53 +0200

    Refactored and made it simpler

--- UnitMaker/ShipMaker.py
+++ UnitMaker/ShipMaker.py
@@ -1,32 +1,21 @@
 ShipNames = ['Frigate','Corvette','Cruiser','Galleon','Battleship','Battle Cruiser',
              'Assault Cruiser','Destroyer','Dreadnought','Man-o-war','Titan','WorldShip','BattleStar','Space Stations']
-Types = {'Hull':'H','Armor':'A','Shield':'S'}
-Weapons = ['Lasers','Neutron Torpedos','Mass Driver','Guided Missiles','Nuclear Missiles','Giga Cannon',
-           'Perdition Beam','Black Hole Ballista']
-StrengthMean = {'Frigate':10, 'Corvette':12, 'Cruiser':25,'Galleon':30,'BattleShip':40,'Battle Cruiser':50,'Swarm Carrier':80,
-                'Assault Cruiser:':100 , 'Destroyer':120 , 'Dreagnough':150,'Man-o-war':200,'Titan':500,'WorldShip':1500,'BattleStar':5000,
-                'Space Station':50}
-WeaponMean = {'Lasers':('A',0.2),'Neutran Torpedos':('A',0.1,'S',0.3),'Mass Driver':('A',0.2,'2',0.4),'Guided Missiles':('A',0.2,'0,1 ',0.4),
-              'Nuclear Missiles':('A',0.3,'A',0.5),'Giga Cannon':('A',0.3,'3,4',0.5),'Perdition Beam':('A',0.6),'Black Hole Ballista':('A',0.8)}
-
+StrengthMean = {'Frigate':(2,10), 'Corvette':(2,12), 'Cruiser':(5,25),'Galleon':(8,30),'BattleShip':(10,40),'Battle Cruiser':(15,50),
+                'Swarm Carrier':(10,80),'Assault Cruiser:':(20,100), 'Destroyer':(40,120), 'Dreagnough':(60,150),
+                'Man-o-war':(50,200),'Titan':(120,500),'WorldShip':(300,1500),'BattleStar':(1000,5000),'Space Station':(10,50)}
 
 class Ship():
 
-    def __init__(self,Strength,Mul,Range,Speed,BC,MC,Type,Effect):
-        self.Strength = Strength
-        self.Mul = Mul
+    def __init__(self,Name,Attack,Health,Range,Speed,BC):
+        self.Name = Name
+        self.Attack = Attack
+        self.Health = Health
         self.Range = Range
         self.Speed = Speed
         self.BC = BC
-        self.Type = Type
-        self.Effect = Effect
-    def setName(self,Name):
-        self.Name = Name
-
 
 
 
 
 
 def GenerateShip():
-    
