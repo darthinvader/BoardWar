167cafc85cc6ad2c1b693cd743bc1e38369e81ad
darthinvader <psaradakisJ@gmail.com>
2018-03-23 19:29:20 +0200

    Refactored the Ships

--- Generators/ShipMaker.py
+++ Generators/ShipMaker.py
@@ -1,77 +1,45 @@
 from numpy.random import normal
 from numpy import random
-from math import ceil
+from numpy import exp
+from math import ceil,floor,sqrt
 from Generators.Globals import *
+from numpy import log2
 import copy
 
 
-
 class Ship():
-    def __init__(self,Name,Attack,Health,Range,Speed,Cost,Effect):
+    def __init__(self, Name, Strength, BC, WC, Effect):
         self.Name = Name
-        self.Attack = Attack
-        self.Health = Health
-        self.Range = Range
-        self.Speed = Speed
-        self.Cost = Cost
-        self.Effect = Effect
-    def setEffect(self,Effect):
+        self.Strength = Strength
+        self.BC = BC
+        self.WC = WC
         self.Effect = Effect
 
-    def setCost(self,Cost):
-        self.Cost =Cost
+    def setEffect(self, Effect):
+        self.Effect = Effect
+
+    def setCost(self, Cost):
+        self.Cost = Cost
 
     def printShip(self):
-        print(self.Name , ",Attack:", self.Attack,",Health:", self.Health, ",Range:", self.Range,
-              ",Speed:",self.Speed,",Cost:",self.Cost,",Effect:",self.Effect)
+        print(self.Name, ",Strength:", self.Strength, ",BC:", self.BC, ",WC:", self.WC, ",Effect:", self.Effect)
 
 
 def GenerateShip(ShipName):
-    means = StrengthMean[ShipName]
-    attack = -1
-    while attack<=0:
-        attack = normal(means[0],means[1]/8)
-    if attack <=40:
-        attack = ceil(attack)
-    elif attack <=400:
-        attack = ceil(attack/10)*10
-    else:
-        attack = ceil(attack/100)*100
-    health = -1
-    while health<=0:
-        health = normal(means[1],means[1]/6)
-
-    if health <=40:
-        health = ceil(health)
-    elif health<=400:
-        health = ceil(health/10)*10
-    else:
-        health = ceil(health/100)*100
-    if means[2] == 1:
-        range = 1
-    else:
-        range = random.randint(1,means[2])
-    if means[3] == 0:
-        speed = 0
-    elif means[3] == 1:
-        speed = 1
-    else:
-        speed = random.randint(1,means[3])
-    effect = ''
-
-    cost = ceil(attack*health*(1+speed/5)*(1+range/3)/20)
-
-    ship = Ship(ShipName,attack,health,range,speed,cost,effect)
+    mean = StrengthMean[ShipName]
+    strength = ceil(normal(mean, mean / 5))
+    if strength <= 1:
+        strength = 1
+    BC = ceil(normal(mean, mean / 3))
+    if BC <= 1:
+        BC = 1
+    WC = ceil((strength/4)*sqrt(strength/BC))
+    ship = Ship(ShipName, strength, BC, WC, '')
     return ship
 
-def GenerateHero(ship):
-    ship = copy.deepcopy(ship)
-    EffectName = EffectNames[(random.randint(1,len(Effects)))]
-    effect = Effects[EffectName]
 
-    ship.setEffect(EffectName)
-    ship.setCost(ship.Cost*effect)
-    return ship
+# def GenerateHero(ship):
+
 
 def GenerateFleet():
     ships = list()
@@ -79,11 +47,11 @@ def GenerateFleet():
         ships.append(GenerateShip(i))
         ships[-1].printShip()
     print()
-    heroes = list()
-    for i in ships:
-        heroes.append(GenerateHero(i))
-        heroes[-1].printShip()
 
+    # heroes = list()
+    # for i in ships:
+    #     heroes.append(GenerateHero(i))
+    #     heroes[-1].printShip()
 
 
 GenerateFleet()
