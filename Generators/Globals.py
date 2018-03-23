0d593f111281e79ff2818d494cfe47bbfea4ffb8
darthinvader <psaradakisJ@gmail.com>
2018-03-23 13:43:41 +0200

    Refactored Folder

---
+++ Generators/Globals.py
@@ -0,0 +1,9 @@
+ShipNames = ['Frigate','Corvette','Cruiser','Galleon','Battleship','Battle Cruiser',
+             'Assault Cruiser','Destroyer','Dreadnought','Man-o-war','Titan','WorldShip','BattleStar',
+             'Space Station','Station Cannon','Station Beam']
+StrengthMean = {'Frigate':(2,10,1,1), 'Corvette':(2,12,1,1), 'Cruiser':(5,25,1,1),'Galleon':(8,30,1,2),'Battleship':(10,40,2,2),'Battle Cruiser':(15,50,2,2),
+                'Swarm Carrier':(10,80,1,2),'Assault Cruiser':(20,100,2,2), 'Destroyer':(40,120,1,2), 'Dreadnought':(60,150,3,2),
+                'Man-o-war':(50,200,2,3),'Titan':(120,500,3,2),'WorldShip':(300,1500,3,1),'BattleStar':(1000,5000,3,1),
+                'Space Station':(10,50,3,0),'Station Cannon':(10,50,3,0),'Station Beam':(20,20,3,0)}
+EffectNames = ['Rush','Black Hole Ballista','Perdition Beam','Swarm','Core Overload']
+Effects = {'Rush':1.2,'Black Hole Ballista':1.8,'Perdition Beam':1.5,'Swarm':2,'Core Overload':1.5}
