87de16910f6d2648d406af7247c128182fdf6a1e
darthinvader <psaradakisJ@gmail.com>
2018-03-31 16:27:19 +0300

    Added Images and also started auto photo maker

--- ImageGen/Maker.py
+++ ImageGen/Maker.py
@@ -1,6 +1,44 @@
+import numpy as np
 from PIL import Image
 
-#def imageMaker():
+CardSize = (1700, 1200)
+PhotoSize = (1000, 800)
+PhotoPos = (100,100)
 
-size =
-im = Image.new("RGB",)
+def ShipCard(ImagePath):
+    # size = (1700, 1200)
+    ShipImage = Image.new('RGBA', CardSize, 'White')
+    Photo = (Image.open(ImagePath)).resize(PhotoSize, Image.ANTIALIAS)
+    ShipImage.paste(Photo, (100, 0), Photo)
+    ShipImage.show()
+
+    del Photo
+
+
+# Convert Image to array
+
+
+ShipCard("foo.png")
+
+# shape = (1700,1200,4)
+# arr = np.ndarray(shape,)
+#
+# arr = np.array(arr)
+# for i in arr:
+#     for j in i:
+#             j[0] = np.random.normal(0, 50)
+#             if j[0]>255:
+#                 j[0] = 255
+#             if j[0]<0:
+#                 j[0] = 0
+#             j[1] = 0#(np.random.normal(122, 10)%255)
+#             j[2] = 0#(np.random.normal(122, 10)%255)
+#             j[3] = 100
+#
+#
+# print(arr)
+#
+#
+# im = Image.fromarray(np.uint8(arr))
+#
+# im.show()
