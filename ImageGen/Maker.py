47a1c4e934bfb7a510fefa14418cebc800b69deb
darthinvader <psaradakisJ@gmail.com>
2018-04-01 18:00:40 +0300

    Refactoring

--- ImageGen/Maker.py
+++ ImageGen/Maker.py
@@ -9,25 +9,61 @@ NamePos = (100, 70)
 PhotoPos = (100, 270)
 EffStrPos = (100, 1120)
 TextPos = (120, 105)
+NameMaxHeight = 100
+NameMaxWidth = 700
+
+
+
+def AddBg(CardImage,Bg):
+    CardBg = (Image.open(Bg)).resize(CardSize, Image.ANTIALIAS)
+    CardImage.paste(CardBg,NeutralPos,None)
+
+def AddCardBorder(CardImage,CBorder):
+    BorderImage = (Image.open(CBorder)).resize(CardSize, Image.ANTIALIAS)
+    CardImage.paste(BorderImage,NeutralPos,BorderImage)
+
+def AddPhoto(CardImage,Photo):
+    CenterPhoto = (Image.open(Photo)).resize(PhotoSize, Image.ANTIALIAS)
+    CardImage.paste(CenterPhoto,PhotoPos,None)
+
+def AddNameBorder(CardImage,NameBorder):
+    NameBorderImage = Image.open(NameBorder)
+    CardImage.paste(NameBorderImage,NamePos,NameBorderImage)
+
+def AddCenterPhotoBorder(CardImage,PhotoBorder):
+    PhotoCenterBorder = Image.open(PhotoBorder)
+    CardImage.paste(PhotoCenterBorder,PhotoPos,PhotoCenterBorder)
+
+def AddTextBorder(CardImage,TextBorder):
+    TextBorderImage = Image.open(TextBorder)
+    CardImage.paste(TextBorderImage,EffStrPos,TextBorderImage)
+
+def AddName(CardImage,text):
+    FontLoad = FontLoad = ImageFont.truetype(SpaceFont, 100)
+    txtImage = Image.new('RGBA', ShipCard.size, (255, 255, 255, 0))
+    d = ImageDraw.Draw(txtImage)
+    txtsize = d.textsize(text,FontLoad)
+    txtWidth = txtsize[0]/100
+    txtHeight = txtsize[1]/100
+
+    fontWidth = NameMaxWidth/txtWidth
+    fontHeight = NameMaxHeight/txtHeight
+
+
+
 
 def ShipCard(ShipName,ImagePath):
     # size = (1700, 1200)
     FontLoad = ImageFont.truetype(SpaceFont, 100)
 
     ShipCard = Image.new('RGBA', CardSize, 'White')
-    ShipPhoto = (Image.open(ImagePath)).resize(PhotoSize, Image.ANTIALIAS)
-    ShipCardBg = (Image.open(ShipBackground)).resize(CardSize, Image.ANTIALIAS)
-    NameCardBorder = (Image.open(NameBorder))
-    CardBorder = (Image.open(CardOutline))
-    PhotoBorder = (Image.open(PhotoOutline))
-    EffBorder = (Image.open(EffOutline))
 
-    ShipCard.paste(ShipCardBg, NeutralPos, None)
-    ShipCard.paste(ShipPhoto, PhotoPos, None)
-    ShipCard.paste(CardBorder, NeutralPos, CardBorder)
-    ShipCard.paste(NameCardBorder, NamePos, NameCardBorder)
-    ShipCard.paste(PhotoBorder, PhotoPos, PhotoBorder)
-    ShipCard.paste(EffBorder, EffStrPos, EffBorder)
+    AddBg(ShipCard,ShipBackground)
+    AddPhoto(ShipCard,ImagePath)
+    AddCardBorder(ShipCard,CardOutline)
+    AddNameBorder(ShipCard,NameBorder)
+    AddCenterPhotoBorder(ShipCard,PhotoOutline)
+    AddTextBorder(ShipCard,EffOutline)
 
     txtImage = Image.new('RGBA', ShipCard.size, (255, 255, 255, 0))
     d = ImageDraw.Draw(txtImage)
